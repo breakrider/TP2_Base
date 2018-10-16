a48ecae70d21e53d27405d358b8d4fcb72fc03aa
maxime <maximumdion@hotmail.com>
2018-10-16 13:18:01 -0400

    16 octobre 2
    etape 6 presque

--- TP2_etape_6.sql
+++ TP2_etape_6.sql
@@ -1,9 +1,33 @@
-
+#1
 select concat(tbl_usager.nom_usager, " ", tbl_usager.prenom_usager) as nom_prenom,
 tbl_local.nom_local,
 tbl_poste.desc_poste
 from tbl_usager
+left join tbl_poste
+on tbl_usager.pk_id_usager= tbl_poste.pk_id_poste
+inner join tbl_local
+on tbl_usager.fk_tbl_local_id_local = tbl_local.pk_id_local;
+
+#2
+select
+(select count(*) from tbl_poste_has_tbl_usager)  as nombre_de_poste_assigner ,
+((select count(*) from tbl_poste)-(select count(*) from tbl_poste_has_tbl_usager))as nombre_de_poste_non_assigner;
+
+#4
+select (nom_local),
+count(*) from tbl_local
+inner join tbl_usager on tbl_local.pk_id_local = tbl_usager.fk_tbl_local_id_local
+#inner join tbl_poste_has_tbl_usager on tbl_usager.pk_id_usager = tbl_usager.pk_id_usager
+group by nom_local;
+
+#5
+select 
+tbl_local.nom_local,
+count(*) as nombre_usager_par_local
+from tbl_usager
+left join tbl_poste
+on tbl_usager.pk_id_usager= tbl_poste.pk_id_poste
 inner join tbl_local
 on tbl_usager.fk_tbl_local_id_local = tbl_local.pk_id_local
-inner join tbl_poste
-on tbl_usager.pk_id_usager= tbl_poste.pk_id_poste;
+group by tbl_local.nom_local;
+
--- test.sql
+++ test.sql
@@ -1,11 +1,31 @@
 
 #show create table tbl_peripherique;
 
+
+#select count(*) from tbl_poste_has_tbl_logiciel where tbl_poste_has_tbl_logiciel.fk_tbl_logiciel_id_logiciel= 1 ;
+#select count(*) from tbl_poste_has_tbl_logiciel where tbl_poste_has_tbl_logiciel.fk_tbl_logiciel_id_logiciel= 22 ;
+
+SELECT pk_id_poste, COUNT(*) AS NombreDePostes
+FROM tbl_usager as u
+inner JOIN tbl_poste_has_tbl_usager AS tphu
+    ON u.pk_id_usager = tphu.fk_tbl_poste_id_poste
+inner JOIN tbl_poste as p ON pk_id_poste = tphu.fk_tbl_usager_id_usager
+GROUP BY fk_tbl_local_id_local
+ORDER BY NombreDePostes DESC;
+
 select concat(tbl_usager.nom_usager, " ", tbl_usager.prenom_usager) as nom_prenom,
 tbl_local.nom_local,
-tbl_type_usager.nom_type_usager
+tbl_poste.desc_poste
 from tbl_usager
+left join tbl_poste
+on tbl_usager.pk_id_usager= tbl_poste.pk_id_poste
 inner join tbl_local
-on tbl_usager.fk_tbl_local_id_local = tbl_local.pk_id_local
-inner join tbl_type_usager
-on tbl_usager.type_usager = tbl_type_usager.pk_id_type_usager;
+on tbl_usager.fk_tbl_local_id_local = tbl_local.pk_id_local;
+
+
+
+select (nom_local),
+count(*) from tbl_local
+inner join tbl_usager on tbl_local.pk_id_local = tbl_usager.fk_tbl_local_id_local
+inner join tbl_poste_has_tbl_usager on tbl_usager.pk_id_usager = tbl_usager.pk_id_usager
+group by nom_local;
